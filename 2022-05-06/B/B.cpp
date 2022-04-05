4652390daa6c3085832f3ad73ea94e0e72e74309
Luis H. P. Mendes <luishpmendes@gmail.com>
2022-04-05 14:12:32 -0300

    CodeForces 1339A - Filling Diamonds
    Signed-off-by: Luis H. P. Mendes <luishpmendes@gmail.com>

---
+++ 2022-05-06/B/B.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+    unsigned t, n;
+
+    while (std::cin >> t) {
+        while (t--) {
+            std::cin >> n;
+            std::cout << n << std::endl;
+        }
+    }
+
+    return 0;
+}
